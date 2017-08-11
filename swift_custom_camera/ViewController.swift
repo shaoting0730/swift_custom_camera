10f2ae5aa190ba53e8161638ae91cc4de4bb3694
周少停 <zst@9elelphas.com>
2017-08-11 10:05:20 +0800

    Initial Commit

---
+++ swift_custom_camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_custom_camera
+//
+//  Created by Shaoting Zhou on 2017/8/11.
+//  Copyright © 2017年 Shaoting Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
