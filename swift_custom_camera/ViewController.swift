e35a6ea63373bf2908d073138f10212ccff1a8e4
周少停 <zst@9elelphas.com>
2017-08-11 16:32:19 +0800

    first commit
    first commit

--- swift_custom_camera/ViewController.swift
+++ swift_custom_camera/ViewController.swift
@@ -7,19 +7,207 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+//屏幕宽高
+struct kScreenWH {
+    static let  width = UIScreen.main.bounds.size.width
+    static let height = UIScreen.main.bounds.size.height
+}
 
+class ViewController: UIViewController,UIAlertViewDelegate {
+    var device:AVCaptureDevice!   //获取设备:如摄像头
+    var input:AVCaptureDeviceInput!   //输入流
+    var photoOutput:AVCaptureStillImageOutput! //输出流
+    var  output:AVCaptureMetadataOutput! //当启动摄像头开始捕获输入
+    var  session:AVCaptureSession!//会话,协调着intput到output的数据传输,input和output的桥梁
+    var  previewLayer:AVCaptureVideoPreviewLayer! //图像预览层，实时显示捕获的图像
+    
+    var photoButton: UIButton?   //拍照按钮
+    var imageView: UIImageView?   //拍照后的成像
+    var image: UIImage?   //拍照后的成像
+    var isJurisdiction: Bool?   //是否获取了拍照标示
+    var flashBtn:UIButton?  //闪光灯按钮
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        isJurisdiction = canUserCamear()
+        if isJurisdiction! {
+            customCamera()
+            customUI()
+        }
+        else {
+            return
+        }
+        
+    }
+    
+    //MARK: 初始化自定义相机
+    func customCamera(){
+       guard let devices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) as? [AVCaptureDevice] else { return } //初始化摄像头设备
+        guard let device = devices.filter({ return $0.position == .back }).first else{ return}
+        self.device = device
+        //输入流初始化
+        self.input = try? AVCaptureDeviceInput(device: device)
+        //照片输出流初始化
+        self.photoOutput = AVCaptureStillImageOutput.init()
+        //输出流初始化
+        self.output = AVCaptureMetadataOutput.init()
+        //生成会话
+        self.session = AVCaptureSession.init()
+        if(self.session.canSetSessionPreset("AVCaptureSessionPreset1280x720")){
+            self.session.sessionPreset = "AVCaptureSessionPreset1280x720"
+        }
+        if(self.session.canAddInput(self.input)){
+            self.session.addInput(self.input)
+        }
+        if(self.session.canAddOutput(self.photoOutput)){
+            self.session.addOutput(self.photoOutput)
+        }
+        //使用self.session，初始化预览层，self.session负责驱动input进行信息的采集，layer负责把图像渲染显示
+        self.previewLayer = AVCaptureVideoPreviewLayer.init(session: self.session)
+        self.previewLayer.frame  = CGRect.init(x: 0, y: 0, width: kScreenWH.width, height: kScreenWH.height)
+        self.previewLayer.videoGravity = "AVLayerVideoGravityResizeAspectFill"
+        self.view.layer.addSublayer(self.previewLayer)
+        //启动
+        self.session.startRunning()
+        if ((try? device.lockForConfiguration()) != nil) {
+            if device.isFlashModeSupported(.auto) {
+                device.flashMode = .auto
+            }
+            //自动白平衡
+            if device.isWhiteBalanceModeSupported(.autoWhiteBalance) {
+                device.whiteBalanceMode = .autoWhiteBalance
+            }
+            device.unlockForConfiguration()
+        }
+        
+        //闪光灯
+        do{ try device.lockForConfiguration() }catch{ }
+        if device.hasFlash == false { return }
+        device.flashMode = AVCaptureFlashMode.auto
+        device.unlockForConfiguration()
+        
+    }
+    
+    // MARK: - 检查相机权限
+    func canUserCamear() -> Bool {
+        let authStatus: AVAuthorizationStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        if authStatus == .denied {
+            let alertView = UIAlertView(title: "请打开相机权限", message: "设置-隐私-相机", delegate: self, cancelButtonTitle: "确定", otherButtonTitles: "取消")
+            alertView.tag = 100
+            alertView.show()
+            return false
+        }
+        else {
+            return true
+        }
+        return true
+    }
+ //MARK: 添加自定义按钮等UI
+    func customUI(){
+       //前后摄像头切换
+        let changeBtn = UIButton.init()
+        changeBtn.frame = CGRect.init(x: kScreenWH.width - 50, y: 20, width: 40, height: 40)
+        changeBtn.setImage(#imageLiteral(resourceName: "change"), for: .normal)
+        changeBtn.addTarget(self, action: #selector(self.changeCamera), for: .touchUpInside)
+        view.addSubview(changeBtn)
+        
+        //拍照按钮
+        photoButton = UIButton(type: .custom)
+        photoButton?.frame = CGRect(x: kScreenWH.width * 1 / 2.0 - 30, y: kScreenWH.height - 100, width: 60, height: 60)
+        photoButton?.setImage(UIImage(named: "photograph"), for: .normal)
+        photoButton?.setImage(UIImage(named: "photograph_Select"), for: .normal)
+        photoButton?.addTarget(self, action: #selector(self.shutterCamera), for: .touchUpInside)
+        view.addSubview(photoButton!)
+        
+      //闪光灯按钮
+        flashBtn = UIButton.init()
+        flashBtn?.frame = CGRect.init(x: 10, y: 20, width: 40, height: 40)
+        flashBtn?.addTarget(self, action: #selector(self.flashAction), for: .touchUpInside)
+        flashBtn?.setImage(#imageLiteral(resourceName: "flash-A"), for: .normal)
+        view.addSubview(flashBtn!)
+        
+       //取消
+        let cancelBtn = UIButton.init()
+        cancelBtn.frame = CGRect.init(x: 10, y: kScreenWH.height - 100, width: 60, height: 60)
+        cancelBtn.setTitle("取消", for: .normal)
+        cancelBtn.addTarget(self, action: #selector(self.cancelActin), for: .touchUpInside)
+        view.addSubview(cancelBtn)
+    }
+    //MARK:前后摄像头更改事件
+    func changeCamera(){
+        //获取之前的镜头
+        guard var position = input?.device.position else { return }
+        //获取当前应该显示的镜头
+        position = position == .front ? .back : .front
+        //创建新的device
+        guard let devices = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) as? [AVCaptureDevice] else { return }
+        // 1.2.取出获取前置摄像头
+        let d = devices.filter({ return $0.position == position }).first
+        device = d
+        //input
+        guard let videoInput = try? AVCaptureDeviceInput(device: d) else { return }
+
+        //切换
+        session.beginConfiguration()
+        session.removeInput(self.input!)
+        session.addInput(videoInput)
+        session.commitConfiguration()
+        self.input = videoInput
+        
+    }
+    //MARK:拍照按钮点击事件
+    func shutterCamera(){
+        let videoConnection: AVCaptureConnection? = photoOutput.connection(withMediaType: AVMediaTypeVideo)
+        if videoConnection == nil {
+            print("take photo failed!")
+            return
+        }
+        photoOutput.captureStillImageAsynchronously(from: videoConnection ?? AVCaptureConnection(), completionHandler: {(_ imageDataSampleBuffer: CMSampleBuffer?, _ error: Error?) -> Void in
+            if imageDataSampleBuffer == nil {
+                return
+            }
+            let imageData: Data? = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataSampleBuffer)   //照片数据流
+            self.image = UIImage(data: imageData!)
+            self.session.stopRunning()
+            self.imageView = UIImageView(frame: self.previewLayer.frame)
+            self.view.insertSubview(self.imageView!, belowSubview: self.photoButton!)
+            self.imageView?.layer.masksToBounds = true
+            self.imageView?.image = self.image
+            print("image size = \(NSStringFromCGSize((self.image?.size)!))")
+        })
+    }
+    //MARK: 闪光灯开关
+    func flashAction(){
+        try? device.lockForConfiguration()
+        switch device.flashMode.rawValue {
+        case 0:
+            device!.flashMode = AVCaptureFlashMode.on
+            flashBtn?.setImage(#imageLiteral(resourceName: "flash-on"), for: .normal)
+            break
+        case 1:
+        device!.flashMode = AVCaptureFlashMode.auto
+        flashBtn?.setImage(#imageLiteral(resourceName: "flash-A"), for: .normal)
+            break
+        default:
+         device!.flashMode = AVCaptureFlashMode.off
+            flashBtn?.setImage(#imageLiteral(resourceName: "flash-off"), for: .normal)
+        }
+        device.unlockForConfiguration()
+        
+    }
+    //MARK:取消按钮
+    func cancelActin(){
+        self.imageView?.removeFromSuperview()
+        self.session.startRunning()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
